b390287147531a4f2ba37713470bee1aa1b13721
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-19 21:15:48 +0530

    Create 310-MinimumHeightTrees

---
+++ 310-MinimumHeightTrees/310-MinimumHeightTrees.cpp
@@ -0,0 +1,51 @@
+// Last updated: 19/03/2026, 21:15:13
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1)
+            return {0};
+
+        vector<vector<int>> adj(n);
+        vector<int> degree(n);
+
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+            degree[e[0]]++;
+            degree[e[1]]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 1)
+                q.push(i);
+        }
+
+        int remaining = n;
+
+        while (remaining > 2) {
+            int size = q.size();
+            remaining -= size;
+
+            while (size--) {
+                int leaf = q.front();
+                q.pop();
+
+                for (int nei : adj[leaf]) {
+                    degree[nei]--;
+                    if (degree[nei] == 1)
+                        q.push(nei);
+                }
+            }
+        }
+
+        vector<int> result;
+        while (!q.empty()) {
+            result.push_back(q.front());
+            q.pop();
+        }
+
+        return result;
+    }
+};

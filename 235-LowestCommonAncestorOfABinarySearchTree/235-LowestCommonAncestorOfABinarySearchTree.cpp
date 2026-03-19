8749f08dd884bce170756efafbdbf87fd6b1bccd
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-19 21:16:09 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.cpp
@@ -0,0 +1,18 @@
+// Last updated: 19/03/2026, 21:15:34
+class Solution {
+  
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        if(root==p||root==q)return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        return left ? left : right;
+        
+       
+    }
+
+    
+    
+};

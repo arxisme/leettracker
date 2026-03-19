6a9640b379b5c0e02e00cb798d50b279c8f973f3
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-19 21:19:47 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,15 @@
+// Last updated: 19/03/2026, 21:19:12
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0 ; i< nums.size(); i++){
+            if(mp.find(target-nums[i])!= mp.end()){
+                return {mp[target-nums[i]], i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {-1,-1};
+        
+    }
+};

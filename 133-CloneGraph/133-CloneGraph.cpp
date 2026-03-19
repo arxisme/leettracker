42445097b898ad874b31e8ca3061193226d598bb
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-19 21:17:14 +0530

    Create 133-CloneGraph

---
+++ 133-CloneGraph/133-CloneGraph.cpp
@@ -0,0 +1,50 @@
+// Last updated: 19/03/2026, 21:16:39
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return nullptr;
+
+        unordered_map<Node*, Node*> mp;
+        queue<Node*> q;
+
+        mp[ node] = new Node(node->val);
+        q.push(node);
+        while (q.size()) {
+            Node* curr = q.front();
+            q.pop();
+            for (auto& nei : curr->neighbors) {
+                if (mp.find(nei) == mp.end()) {
+                    mp[nei] = new Node(nei->val);
+                    q.push(nei);
+                }
+                
+                mp[curr]->neighbors.push_back(mp[nei]);
+                
+            }
+        }
+
+        return mp[node];
+    }
+};

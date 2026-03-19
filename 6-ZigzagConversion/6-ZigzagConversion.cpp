79ae00b1135d7e0c83d418c8ab738aaa0df5f0c1
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-20 00:23:09 +0530

    File updated at 20/03/2026, 00:22:34

--- 6-ZigzagConversion/6-ZigzagConversion.cpp
+++ 6-ZigzagConversion/6-ZigzagConversion.cpp
@@ -1,50 +1,27 @@
-// Last updated: 20/03/2026, 00:13:48
-1class Solution {
-2public:
-3    string convert(string s, int numRows) {
-4        if(numRows==1)return s;
-5        string row = "";
-6        int n = s.size();
-7       
-8        vector<string> mat(numRows);
-9        
-10        int k = 0;
-11        int i=0;
-12        int j =0;
-13        int per =1;
-14        int dia =0;
-15
-16        while(k!=n){
-17            mat[i]+=s[k];
-18            if(i==mat.size()-1 ){
-19                dia=1;
-20                per=0;
-21
-22            }
-23            if(i==0){
-24                dia =0;
-25                per =1;
-26            }
-27            if(per){
-28                i++;
-29            }
-30            if(dia){
-31                i--;
-32                j++;
-33            }
-34            k++;
-35
-36
-37        }
-38         k =n;
-39        string res="";
-40        for(auto &row: mat){
-41            res+=row;
-42        }
-43        
-44        return res;
-45
-46
-47        
-48    }
-49};
+// Last updated: 20/03/2026, 00:22:34
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        // If only one row or string is short, no zigzag happens
+        if (numRows == 1 || s.length() <= numRows) return s;
+
+        vector<string> rows(min(numRows, int(s.length())));
+        int currRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currRow] += c;
+            // Reverse direction when we hit the top or bottom
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (string row : rows) {
+            result += row;
+        }
+        return result;
+    }
+};

d46639a328e22615dd4f537a9a7cd7d1b369fc55
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-20 21:54:29 +0530

    File updated at 20/03/2026, 21:53:54

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,16 +1,15 @@
-// Last updated: 20/03/2026, 21:53:24
+// Last updated: 20/03/2026, 21:53:54
 1class Solution {
 2public:
 3    string longestCommonPrefix(vector<string>& strs) {
 4        string prefix = strs[0];
 5
-6        for(int i = 0;i<strs.size();i++){
-7            while(strs[i].find(prefix)!=0){
+6        for (int i = 1; i < strs.size(); i++) {
+7            while (strs[i].find(prefix) != 0) {
 8                prefix.pop_back();
 9            }
-10            
-11        }
-12
-13        return prefix;
-14    }
-15};
+10        }
+11
+12        return prefix;
+13    }
+14};

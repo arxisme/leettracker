54400553eb83ea00a1630e93c96f689c44dd2c11
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-21 16:41:31 +0530

    File updated at 21/03/2026, 16:40:55

--- 134-GasStation/134-GasStation.cpp
+++ 134-GasStation/134-GasStation.cpp
@@ -1,22 +1,23 @@
-// Last updated: 19/03/2026, 21:16:37
-class Solution {
-public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total = 0;     
-        int tank = 0;       
-        int start = 0;
-
-        for (int i = 0; i < (int)gas.size(); ++i) {
-            int diff = gas[i] - cost[i];
-            total += diff;
-            tank  += diff;
-
-            if (tank < 0) {
-                tank = 0;
-                start = i + 1;
-            }
-        }
-
-        return (total >= 0) ? start : -1;
-    }
-};
+// Last updated: 21/03/2026, 16:40:55
+1class Solution {
+2public:
+3    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+4        int total = 0;     
+5        int tank = 0;       
+6        int start = 0;
+7
+8        for (int i = 0; i < (int)gas.size(); ++i) {
+9            int diff = gas[i] - cost[i];
+10            total += diff;
+11            tank  += diff;
+12
+13            if (tank < 0) {
+14                tank = 0;
+15                start = i + 1;
+16            }
+17        }
+18
+19        return (total >= 0) ? start : -1;
+20    }
+21};
+22

a8765d706558340f8962b6bfdbaf3f9aa9fd672e
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-21 17:01:57 +0530

    File updated at 21/03/2026, 17:01:21

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
@@ -1,32 +1,26 @@
-// Last updated: 21/03/2026, 16:52:37
+// Last updated: 21/03/2026, 17:01:21
 1class Solution {
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
-4        long long  p = 1;
-5        int z =0;
-6        vector<int> res(nums.size(),0);
-7        vector<int> before(nums.size(), 1);
-8        vector<int> after(nums.size(),1);
-9        int b=1;
-10        int a=1;
-11        for(int i = 0 ; i<nums.size(); i++){
-12            b*=nums[i];
-13            a*=nums[nums.size()-1-i];
-14            before[i]=b;
-15            after[nums.size()-1-i]=a;
-16
-17        }
-18        res[0]=after[1];
-19        res[nums.size()-1]=before[nums.size()-2];
-20        for(int i = 1 ; i<nums.size()-1; i++){
-21           res[i]=before[i-1]*after[i+1];
+4        if(nums.size()==1)return nums;
+5        int n = nums.size();
+6        vector<int> pre(n,1);
+7        vector<int> after(n,1);
+8        for(int i =0;i<n;i++){
+9            pre[i]=i-1+1?pre[i-1]*nums[i]:nums[i];
+10            after[n-i-1]=n-i<n?after[n-i]*nums[n-i-1]:nums[n-i-1];
+11
+12        }
+13        vector<int> res(n,1);
+14        res[0]=after[1];
+15        res[n-1]=pre[n-2];
+16        for(int i = 1; i<n-1;i++){
+17            res[i]=after[i+1]*pre[i-1];
+18
+19
+20        }
+21        return res;
 22
-23        }
-24
-25
-26
-27        
-28      
-29        return res;
-30    }
-31};
+23       
+24    }
+25};

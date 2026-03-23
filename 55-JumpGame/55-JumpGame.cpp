2f92133c1623841f3454fe26fcd2e6c0e9ed6a63
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-24 00:10:41 +0530

    File updated at 24/03/2026, 00:10:05

--- 55-JumpGame/55-JumpGame.cpp
+++ 55-JumpGame/55-JumpGame.cpp
@@ -1,25 +1,20 @@
-// Last updated: 19/03/2026, 21:18:13
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
-        for(int i = nums.size()-2; i>=0;  i--){
-            if(nums[i]==0){
-                int j = i;
-                i--;
-                if(i<0)return false;
-                for(;i>=0;i--){
-                    if(nums[i]>=j-i+1){
-                        break;
-                    }
-                    else if(i==0){
-                        return false;
-                    }
-
-                }
-
-            }
-        }
-        return true;
-        
-    }
-};
+// Last updated: 24/03/2026, 00:10:05
+1class Solution {
+2public:
+3    bool canJump(vector<int>& nums) {
+4        if(nums.size()==1)return 1;
+5        
+6        int n = nums.size();
+7        int minStep =1;
+8        for(int i = n-2; i>0; i--){
+9            if(nums[i]<minStep){
+10                minStep++;
+11                
+12            }
+13            else{
+14                minStep=1;
+15            }
+16        }
+17        return nums[0]>=minStep;
+18    }
+19};

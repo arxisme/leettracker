cb784cbf5a69ee9493dbb5bb36548a37851d9615
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-23 17:04:28 +0530

    File updated at 23/03/2026, 17:03:51

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,17 +1,27 @@
-// Last updated: 19/03/2026, 21:16:10
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int target = nums.size() / 2; 
-        unordered_map<int, int> map;
-
-        for (int i = 0; i < nums.size(); i++) {
-            map[nums[i]]++;
-            if (map[nums[i]] > target) {
-                return nums[i];
-            }
-        }
-
-        return 0; // This line is theoretically unreachable if a majority element is guaranteed
-    }
-};
+// Last updated: 23/03/2026, 17:03:51
+1class Solution {
+2public:
+3    int majorityElement(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5        int res = 1;
+6        int f = 0;
+7        int result = nums[0];
+8        for(int i = 1; i<nums.size(); i++){
+9            if(nums[i]==nums[i-1]){
+10                res++;
+11                if(f<res){
+12                    f = res;
+13                    result = nums[i];
+14
+15                }
+16
+17            }else{
+18                res =1;
+19            }
+20        }
+21        return result;
+22
+23
+24        
+25    }
+26};

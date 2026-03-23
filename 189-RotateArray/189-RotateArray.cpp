22aeb05b58dd354cc5a13ae4a30ad57dee59e692
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-23 20:18:39 +0530

    File updated at 23/03/2026, 20:18:03

--- 189-RotateArray/189-RotateArray.cpp
+++ 189-RotateArray/189-RotateArray.cpp
@@ -1,15 +1,24 @@
-// Last updated: 23/03/2026, 18:28:37
+// Last updated: 23/03/2026, 20:18:03
 1class Solution {
 2public:
 3    void rotate(vector<int>& nums, int k) {
 4        int n = nums.size();
-5        vector<int> re = nums;
-6        k = k%n;
-7
-8        for (int i = 0; i < n; i++) {
-9            nums[i] = re[(i - k+n) % n];
-10        }
-11
-12        return;
-13    }
-14};
+5        k %= n;
+6
+7        int count = 0;
+8
+9        for (int start = 0; count < n; start++) {
+10            int current = start;
+11            int prev = nums[start];
+12
+13            do {
+14                int next = (current + k) % n;
+15                int temp = nums[next];
+16                nums[next] = prev;
+17                prev = temp;
+18                current = next;
+19                count++;
+20            } while (start != current);
+21        }
+22    }
+23};

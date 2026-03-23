cb92e4461d7d42202755741436a8a47599264d0d
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-24 01:42:24 +0530

    File updated at 24/03/2026, 01:41:48

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,4 +1,4 @@
-// Last updated: 24/03/2026, 01:38:09
+// Last updated: 24/03/2026, 01:41:48
 1class Solution {
 2public:
 3    int maxArea(vector<int>& height) {
@@ -6,10 +6,19 @@
 5        int i = 0;
 6        int j = height.size()-1;
 7        while(i<j){
-8            res = max(res, min(height[i],height[j])*(j-i));
-9            if(height[i]<=height[j])i++;
-10            else j--;
-11        }
-12        return res;
-13    }
-14};
+8           int h ;
+9            if(height[i]<=height[j]){
+10                h = height[i];
+11                i++;
+12                
+13            }
+14            else {
+15                h = height[j--];
+16
+17            }
+18            res =max(res, h*(j-i+1));
+19            
+20        }
+21        return res;
+22    }
+23};

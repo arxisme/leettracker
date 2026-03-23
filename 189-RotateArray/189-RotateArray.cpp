bec0a1a09ded3551907477b32e0dd9a2f0e182e6
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-23 23:02:11 +0530

    File updated at 23/03/2026, 23:01:35

--- 189-RotateArray/189-RotateArray.cpp
+++ 189-RotateArray/189-RotateArray.cpp
@@ -1,28 +1,12 @@
-// Last updated: 23/03/2026, 20:56:59
+// Last updated: 23/03/2026, 23:01:35
 1class Solution {
 2public:
 3    void rotate(vector<int>& nums, int k) {
 4        int n = nums.size();
-5        k = k % n;
+5        k %= n;
 6
-7        int g = 0;
-8        int j = __gcd(n, k);   // FIX
-9
-10        while (j--) {
-11            int t = g++;
-12            int start = t;
-13            int s = nums[t];
-14
-15            t = (t + k) % n;
-16
-17            while (t != start) {
-18                int temp = nums[t];
-19                nums[t] = s;
-20                s = temp;
-21                t = (t + k) % n;
-22            }
-23
-24            nums[start] = s;
-25        }
-26    }
-27};
+7        reverse(nums.begin(), nums.end());
+8        reverse(nums.begin(), nums.begin() + k);
+9        reverse(nums.begin() + k, nums.end());
+10    }
+11};

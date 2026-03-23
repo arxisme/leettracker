03ee5e2d96fbeafc9121d65f5bab10b2866a1ac3
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-23 18:29:14 +0530

    File updated at 23/03/2026, 18:28:37

--- 189-RotateArray/189-RotateArray.cpp
+++ 189-RotateArray/189-RotateArray.cpp
@@ -1,25 +1,15 @@
-// Last updated: 19/03/2026, 21:16:08
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        int size = nums.size();
-        if (size == 0) return;
-
-        k = k % size;  // Handle cases where k > size
-        
-        if (k == 0) return;
-
-        // Reverse the entire array
-        reverse(nums.begin(), nums.end());
-
-        // Reverse the first k elements
-        reverse(nums.begin(), nums.begin() + k);
-
-        // Reverse the remaining elements
-        reverse(nums.begin() + k, nums.end());
-    }
-};
+// Last updated: 23/03/2026, 18:28:37
+1class Solution {
+2public:
+3    void rotate(vector<int>& nums, int k) {
+4        int n = nums.size();
+5        vector<int> re = nums;
+6        k = k%n;
+7
+8        for (int i = 0; i < n; i++) {
+9            nums[i] = re[(i - k+n) % n];
+10        }
+11
+12        return;
+13    }
+14};

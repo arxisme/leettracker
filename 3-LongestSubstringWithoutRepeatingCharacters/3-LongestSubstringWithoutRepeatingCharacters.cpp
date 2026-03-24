b51fc6f8d0605397e71a057d7a8f55b16663dc8a
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-24 15:14:54 +0530

    File updated at 24/03/2026, 15:14:16

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,17 +1,21 @@
-// Last updated: 19/03/2026, 21:19:09
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> last;
-        int i = 0, ans = 0;
-
-        for (int j = 0; j < s.size(); j++) {
-            if (last.count(s[j]) && last[s[j]] >= i) {
-                i = last[s[j]] + 1;
-            }
-            last[s[j]] = j;
-            ans = max(ans, j - i + 1);
-        }
-        return ans;
-    }
-};
+// Last updated: 24/03/2026, 15:14:16
+1class Solution {
+2public:
+3    int lengthOfLongestSubstring(string s) {
+4        vector<int> last(256, -1);
+5        
+6        int res = 0;
+7        int start = 0;
+8
+9        for (int i = 0; i < s.size(); i++) {
+10            if (last[s[i]] >= start) {
+11                start = last[s[i]] + 1;
+12            }
+13
+14            last[s[i]] = i;
+15            res = max(res, i - start + 1);
+16        }
+17
+18        return res;
+19    }
+20};

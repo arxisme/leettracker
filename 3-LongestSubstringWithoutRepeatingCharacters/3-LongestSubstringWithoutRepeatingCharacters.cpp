1a45ea0b8683ae6d318bece66d99fe1162ffda82
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-26 13:22:32 +0530

    File updated at 26/03/2026, 13:21:53

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,21 +1,20 @@
-// Last updated: 24/03/2026, 15:14:16
+// Last updated: 26/03/2026, 13:21:53
 1class Solution {
 2public:
 3    int lengthOfLongestSubstring(string s) {
-4        vector<int> last(256, -1);
-5        
-6        int res = 0;
-7        int start = 0;
-8
-9        for (int i = 0; i < s.size(); i++) {
-10            if (last[s[i]] >= start) {
-11                start = last[s[i]] + 1;
-12            }
-13
-14            last[s[i]] = i;
-15            res = max(res, i - start + 1);
-16        }
-17
-18        return res;
-19    }
-20};
+4        vector<int> last(256,-1);
+5        int res = 0;
+6        int start = 0;
+7        for(int i =0; i<s.size();i++){
+8            if(last[s[i]]>=start){
+9                start=last[s[i]]+1;
+10              
+11            }
+12            
+13            last[s[i]]=i;
+14            res = max(res,i-start+1);
+15        }
+16        return res;
+17        
+18    }
+19};

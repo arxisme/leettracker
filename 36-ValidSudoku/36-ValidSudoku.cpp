4ce1d995181518e2b51dbc6bd69ce1e305e162d6
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-27 21:39:52 +0530

    File updated at 27/03/2026, 21:39:12

--- 36-ValidSudoku/36-ValidSudoku.cpp
+++ 36-ValidSudoku/36-ValidSudoku.cpp
@@ -1,31 +1,31 @@
-// Last updated: 27/03/2026, 20:56:28
+// Last updated: 27/03/2026, 21:39:12
 1class Solution {
 2public:
 3    bool isValidSudoku(vector<vector<char>>& board) {
-4        vector<unordered_map<int, int>> row(9);
-5        vector<unordered_map<int, int>> col(9);
-6        vector<unordered_map<int, int>> grid(9);
+4        vector<vector<bool>> r(9, vector<bool>(9, 0));
+5        vector<vector<bool>> c(9, vector<bool>(9, 0));
+6        vector<vector<bool>> g(9, vector<bool>(9, 0));
 7
 8        for (int i = 0; i < 9; i++) {
 9            for (int j = 0; j < 9; j++) {
 10                if(board[i][j]=='.')continue;
-11                int num = board[i][j]-'0';
-12                int rn = i;
-13                int gn = (i/3)*3+j/3;
-14                int cn = j;
-15                if (row[rn].count(num))
-16                    return false;
-17                else
-18                    row[rn][num] = 1;
-19                
-20                if (col[cn].count(num))
-21                    return false;
-22                else
-23                    col[cn][num] = 1;
-24                if (grid[gn].count(num))
+11                int num = board[i][j] - '0' - 1;
+12                if (r[i][num])
+13                    return false;
+14                else
+15                    r[i][num] = 1;
+16
+17                if (c[j][num])
+18                    return false;
+19                else
+20                    c[j][num] = 1;
+21
+22                int k = (i / 3) * 3 + (j/3);
+23
+24                if (g[k][num])
 25                    return false;
 26                else
-27                    grid[gn][num] = 1;
+27                    g[k][num] = 1;
 28            }
 29        }
 30        return true;

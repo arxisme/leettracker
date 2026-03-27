1710b598a9e3b8957d9a0d9d27ef0010725a50c8
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-27 21:43:20 +0530

    File updated at 27/03/2026, 21:42:40

--- 36-ValidSudoku/36-ValidSudoku.cpp
+++ 36-ValidSudoku/36-ValidSudoku.cpp
@@ -1,33 +1,29 @@
-// Last updated: 27/03/2026, 21:39:12
+// Last updated: 27/03/2026, 21:42:40
 1class Solution {
 2public:
 3    bool isValidSudoku(vector<vector<char>>& board) {
-4        vector<vector<bool>> r(9, vector<bool>(9, 0));
-5        vector<vector<bool>> c(9, vector<bool>(9, 0));
-6        vector<vector<bool>> g(9, vector<bool>(9, 0));
-7
-8        for (int i = 0; i < 9; i++) {
-9            for (int j = 0; j < 9; j++) {
-10                if(board[i][j]=='.')continue;
-11                int num = board[i][j] - '0' - 1;
-12                if (r[i][num])
-13                    return false;
-14                else
-15                    r[i][num] = 1;
-16
-17                if (c[j][num])
-18                    return false;
-19                else
-20                    c[j][num] = 1;
-21
-22                int k = (i / 3) * 3 + (j/3);
-23
-24                if (g[k][num])
-25                    return false;
-26                else
-27                    g[k][num] = 1;
-28            }
-29        }
-30        return true;
-31    }
-32};
+4        int row[9] = {0}, col[9] = {0}, grid[9] = {0};
+5
+6        for (int i = 0; i < 9; i++) {
+7            for (int j = 0; j < 9; j++) {
+8                if (board[i][j] == '.') continue;
+9
+10                int num = board[i][j] - '1';   // 0 to 8
+11                int mask = 1 << num;
+12
+13                int g = (i / 3) * 3 + (j / 3);
+14
+15                // check
+16                if (row[i] & mask) return false;
+17                if (col[j] & mask) return false;
+18                if (grid[g] & mask) return false;
+19
+20                // set
+21                row[i] |= mask;
+22                col[j] |= mask;
+23                grid[g] |= mask;
+24            }
+25        }
+26        return true;
+27    }
+28};

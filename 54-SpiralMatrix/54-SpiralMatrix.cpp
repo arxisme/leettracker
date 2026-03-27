a8f57ede3cd2be04160109ccc0308146faa93a9f
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-03-28 00:00:36 +0530

    File updated at 27/03/2026, 23:59:56

--- 54-SpiralMatrix/54-SpiralMatrix.cpp
+++ 54-SpiralMatrix/54-SpiralMatrix.cpp
@@ -1,37 +1,40 @@
-// Last updated: 19/03/2026, 21:18:14
-class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        int top = 0, bottom = matrix.size() - 1;
-        int left = 0, right = matrix[0].size() - 1;
-        while(top<= bottom && left<= right){
-            for(int i = left; i<= right ; ++i){
-                result.push_back(matrix[top][i]);
-            }
-            top++;
-
-            for(int i = top ; i<= bottom ; i++){
-                result.push_back(matrix[i][right]);
-            }
-            right--;
-            if(bottom>=top){
-            for(int i = right; i>= left; i--){
-                result.push_back(matrix[bottom][i]);
-
-
-            }
-            bottom--;
-            }
-            if(left<= right){
-            for(int i = bottom; i>= top ; i--){
-                result.push_back(matrix[i][left]);
-            }
-            left++;}
-
-        }
-
-        return result;
-        
-    }
-};
+// Last updated: 27/03/2026, 23:59:56
+1class Solution {
+2public:
+3    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+4        int mini = 0;
+5        int maxi = matrix.size()-1;
+6        int minj = 0;
+7        int maxj = matrix[0].size()-1;
+8        int r = 1;
+9        int c = 0;
+10        vector<int> res;
+11        int i = 0;
+12        int j = 0;
+13        while(mini <=maxi && minj<=maxj ){
+14           for(int j = minj; j<= maxj;j++){
+15            res.push_back(matrix[mini][j]);
+16           }
+17           mini++;
+18           for(int i = mini;i<=maxi;i++){
+19             res.push_back(matrix[i][maxj]);
+20           }
+21           maxj--;
+22          if (mini <= maxi) {
+23                for (int j = maxj; j >= minj; j--)
+24                    res.push_back(matrix[maxi][j]);
+25                maxi--;
+26            }
+27
+28           
+29            if (minj <= maxj) {
+30                for (int i = maxi; i >= mini; i--)
+31                    res.push_back(matrix[i][minj]);
+32                minj++;
+33            }
+34        }
+35        return res;
+36        
+37        
+38    }
+39};

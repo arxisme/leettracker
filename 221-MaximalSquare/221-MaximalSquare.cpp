cd47263f234d4f878aa0c6ca6c6cfefbe91f2380
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-04-04 16:22:08 +0530

    File updated at 04/04/2026, 16:21:19

--- 221-MaximalSquare/221-MaximalSquare.cpp
+++ 221-MaximalSquare/221-MaximalSquare.cpp
@@ -1,26 +1,31 @@
-// Last updated: 04/04/2026, 16:05:03
+// Last updated: 04/04/2026, 16:21:19
 1class Solution {
 2public:
 3    int maximalSquare(vector<vector<char>>& matrix) {
-4        int res = 0;
-5        vector<vector<int>> dp(matrix.size(), vector<int> (matrix[0].size(), 0));
-6        for(int i = 1 ; i< matrix.size(); i++){
-7            dp[i][0]= matrix[i][0]-'0';
-8            res= max(res, dp[i][0]);
-9        }
-10        for(int i = 0 ; i< matrix[0].size(); i++){
-11            dp[0][i]= matrix[0][i]-'0';
-12            res= max(res, dp[0][i]);
-13        }
-14        for(int i = 1; i< matrix.size(); i++){
-15            for(int j = 1; j< matrix[0].size(); j++){
-16                if(matrix[i][j] == '0')continue;
-17                int a = min({dp[i-1][j], dp[i][j-1],dp[i-1][j-1]});
-18                dp[i][j]=a+1;
-19                res = max(a+1, res);
-20            }
-21        }
-22        return res*res;
-23        
-24    }
-25};
+4        int m = matrix.size(), n = matrix[0].size();
+5        int res = 0;
+6
+7       
+8        vector<vector<int>> dp(m, vector<int>(n));
+9
+10        for (int i = 0; i < m; i++) {
+11            for (int j = 0; j < n; j++) {
+12                dp[i][j] = matrix[i][j] - '0';
+13            }
+14        }
+15
+16        for (int i = 0; i < m; i++) res = max(res, dp[i][0]);
+17        for (int j = 0; j < n; j++) res = max(res, dp[0][j]);
+18
+19        for (int i = 1; i < m; i++) {
+20            for (int j = 1; j < n; j++) {
+21                if (dp[i][j] == 1) {
+22                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+23                    res = max(res, dp[i][j]);
+24                }
+25            }
+26        }
+27
+28        return res * res;
+29    }
+30};

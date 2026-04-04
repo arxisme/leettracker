b4858192f755cb5f6537f048587dafd12ea67ac9
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-04-04 16:23:08 +0530

    File updated at 04/04/2026, 16:22:18

--- 221-MaximalSquare/221-MaximalSquare.cpp
+++ 221-MaximalSquare/221-MaximalSquare.cpp
@@ -1,31 +1,27 @@
-// Last updated: 04/04/2026, 16:21:19
-1class Solution {
-2public:
-3    int maximalSquare(vector<vector<char>>& matrix) {
-4        int m = matrix.size(), n = matrix[0].size();
-5        int res = 0;
-6
-7       
-8        vector<vector<int>> dp(m, vector<int>(n));
-9
-10        for (int i = 0; i < m; i++) {
-11            for (int j = 0; j < n; j++) {
-12                dp[i][j] = matrix[i][j] - '0';
-13            }
-14        }
-15
-16        for (int i = 0; i < m; i++) res = max(res, dp[i][0]);
-17        for (int j = 0; j < n; j++) res = max(res, dp[0][j]);
-18
-19        for (int i = 1; i < m; i++) {
-20            for (int j = 1; j < n; j++) {
-21                if (dp[i][j] == 1) {
-22                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-23                    res = max(res, dp[i][j]);
-24                }
-25            }
-26        }
-27
-28        return res * res;
-29    }
-30};
+// Last updated: 04/04/2026, 16:22:18
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.size() == 0) {
+            return 0;
+        }
+        // vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 0));
+        vector<int> prev(matrix[0].size(), 0);
+        vector<int> curr(matrix[0].size(), 0);
+        int ans = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (i == 0 || j == 0 || matrix[i][j] == '0') {
+                    curr[j] = matrix[i][j] - '0';
+                } else {
+                    curr[j] =
+                        min(curr[j - 1], min(prev[j - 1], prev[j])) +
+                        1;
+                }
+                ans = max(curr[j], ans);
+            }
+            prev = curr;
+        }
+        return ans * ans;
+    }
+};

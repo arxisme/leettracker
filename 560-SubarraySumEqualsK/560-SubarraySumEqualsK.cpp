001accf85924b73ac71c913317c5300bb8c82026
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-04-04 18:29:12 +0530

    File updated at 04/04/2026, 18:28:22

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,20 +1,22 @@
-// Last updated: 04/04/2026, 17:54:46
+// Last updated: 04/04/2026, 18:28:22
 1class Solution {
 2public:
-3    int subarraySum(vector<int>& nums, int k) {
-4
-5        unordered_map<int, int> mp;
-6        mp[0] = 1;
-7        int sum = 0;
-8        int res = 0;
-9
-10        for (int i = 0; i < nums.size(); i++) {
-11            sum += nums[i];
-12            
-13            if (mp.find(sum-k) != mp.end())
-14                res += mp[sum-k];
-15            mp[sum]++;
-16        }
-17        return res;
-18    }
-19};
+3    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5        vector<vector<int>> res = {{}};
+6        map<std::vector<int>, int> mp;
+7        mp[{}] = 1;
+8        for (int i = 0; i < nums.size(); i++) {
+9            int s = res.size();
+10            for (int j = 0; j < s; j++) {
+11                vector<int> a = res[j];
+12                a.push_back(nums[i]);
+13                if (mp.find(a) == mp.end()) {
+14                    res.push_back(a);
+15                    mp[a] = 1;
+16                }
+17            }
+18        }
+19        return res;
+20    }
+21};

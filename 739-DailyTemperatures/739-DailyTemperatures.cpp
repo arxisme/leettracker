62819b9b955c4456706f3facda9d6adaedd6988b
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-04-12 20:55:28 +0530

    File updated at 12/04/2026, 20:54:32

--- 739-DailyTemperatures/739-DailyTemperatures.cpp
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -1,21 +1,21 @@
-// Last updated: 19/03/2026, 21:14:32
-class Solution {
-public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> st;
-        vector<int> ans(temperatures.size(), 0);
-        for (int i = temperatures.size() - 1; i >= 0; i--) {
-           
-
-            while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
-                st.pop();
-            }
-            if (!st.empty()) {
-                ans[i] = st.top()- i;
-                
-            }
-            st.push( i);
-        }
-        return ans;
-    }
-};
+// Last updated: 12/04/2026, 20:54:32
+1class Solution {
+2public:
+3    vector<int> dailyTemperatures(vector<int>& temperatures) {
+4        vector<int> res(temperatures.size(), 0);
+5        stack<int> st;
+6        int n = temperatures.size();
+7        st.push(n - 1);
+8        for (int i = n - 2; i >= 0; i--) {
+9
+10            while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
+11                st.pop();
+12            }
+13            if (!st.empty()) {
+14                res[i] = st.top() - i;
+15            }
+16            st.push(i);
+17        }
+18        return res;
+19    }
+20};

c083be5c4ee84db8aecbba44f47fbe851bb28275
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-04-12 23:24:06 +0530

    File updated at 12/04/2026, 23:23:10

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,22 +1,24 @@
-// Last updated: 19/03/2026, 21:14:57
-class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        vector<int> count(26, 0);
-        int maxFreq = 0;
-        int i = 0, ans = 0;
-
-        for (int j = 0; j < s.size(); j++) {
-            count[s[j] - 'A']++;
-            maxFreq = max(maxFreq, count[s[j] - 'A']);
-
-            while ((j - i + 1) - maxFreq > k) {
-                count[s[i] - 'A']--;
-                i++;
-            }
-
-            ans = max(ans, j - i + 1);
-        }
-        return ans;
-    }
-};
+// Last updated: 12/04/2026, 23:23:10
+1class Solution {
+2public:
+3    int characterReplacement(string s, int k) {
+4        vector<int> count(26, 0);
+5        int start = 0;
+6        int maxF = 0;
+7        int res = 0;
+8        for(int i = 0; i<s.size(); i++){
+9            count[s[i]-'A']++;
+10            maxF = max(maxF, count[s[i]-'A']);
+11
+12            while((i-start+1)-maxF >k){
+13                count[s[start]-'A']--;
+14                start++;
+15            }
+16            res = max(res, i-start+1);
+17
+18        }
+19        return res;
+20
+21        
+22    }
+23};

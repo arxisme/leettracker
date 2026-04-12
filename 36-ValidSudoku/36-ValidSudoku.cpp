89e9185ad18bbb652f5de5188e46ea022ef3dae1
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2026-04-12 17:28:56 +0530

    File updated at 12/04/2026, 17:27:59

--- 36-ValidSudoku/36-ValidSudoku.cpp
+++ 36-ValidSudoku/36-ValidSudoku.cpp
@@ -1,24 +1,24 @@
-// Last updated: 12/04/2026, 17:24:26
+// Last updated: 12/04/2026, 17:27:59
 1class Solution {
 2public:
 3    bool isValidSudoku(vector<vector<char>>& board) {
-4        int row[9] = {0}, col[9] = {0}, grid[9] = {0};
-5
-6        for (int i = 0; i < 9; i++) {
-7            for (int j = 0; j < 9; j++) {
-8                if (board[i][j] == '.') continue;
-9
-10                int num = board[i][j] - '1';   // 0 to 8
-11                int mask = 1 << num;
-12
-13                int g = (i / 3) * 3 + (j / 3);
-14
-15                // check
-16                if (row[i] & mask) return false;
-17                if (col[j] & mask) return false;
-18                if (grid[g] & mask) return false;
-19
-20                // set
+4        int col[9] = {0};
+5        int row[9] = {0};
+6        int grid[9] = {0};
+7        for (int i = 0; i < 9; i++) {
+8            for (int j = 0; j < 9; j++) {
+9                if (board[i][j] == '.')
+10                    continue;
+11                int num = board[i][j] - '1';
+12                int g = (i / 3) * 3 + (j / 3);
+13                int mask = 1 << num;
+14                if (row[i] & mask)
+15                    return false;
+16                if (col[j] & mask)
+17                    return false;
+18                if (grid[g] & mask)
+19                    return false;
+20
 21                row[i] |= mask;
 22                col[j] |= mask;
 23                grid[g] |= mask;
